Fix Cluster Frequency vector calculation (divide by sample set size instead of cluster size) 	~ What ratio of a sample set's points are present in each cluster Change back to arcsinh transformation for HVTN data in preprocess_input() Clean up LOO code Clean up path variables in sample_set_classification

scripts/model.py
@@ -10,6 +10,8 @@ from sklearn.svm import SVC
 from sklearn.model_selection import GridSearchCV, train_test_split
 from sklearn.ensemble import RandomForestClassifier
 
+import anndata
+
 
 
 def random_feats(X, gamma=6, frequency_seed=None):
@@ -103,7 +105,6 @@ def get_cluster_freq_vector(km, subsample_data, subsample_preds, num_clusters):
     :param num_clusters: KMeans clusters
     :return: (num_sample_set X num_clusters) cluster freq vec, (num_sample_set, ) sample set labels, sample_sets used
     """
-    cluster_sizes = dict([(i, (subsample_preds == i).sum()) for i in range(num_clusters)])
     try:
         # For NK Cell data
         fcs_filename_key = "fcs_filename"
@@ -114,20 +115,19 @@ def get_cluster_freq_vector(km, subsample_data, subsample_preds, num_clusters):
         sample_sets = subsample_data.obs[fcs_filename_key].unique()
     vec = []
     sample_labels = []
-    for sample in sample_sets:
-        sample_x = subsample_data[subsample_data.obs[fcs_filename_key] == sample, :]
-        sample_label = subsample_data[subsample_data.obs[fcs_filename_key] == sample].obs.label.unique()[0]
-        sample_preds = km.predict(sample_x.X)       # Predicting again to get cluster ID of this sample set's points (same as filtering from subsample_preds)
+    for sample_set_name in sample_sets:
+        sample_set = subsample_data[subsample_data.obs[fcs_filename_key] == sample_set_name, :]
+        sample_set_label = subsample_data[subsample_data.obs[fcs_filename_key] == sample_set_name].obs.label.unique()[0]
+        sample_set_preds = km.predict(sample_set.X)       # Predicting again to get cluster ID of this sample set's points (same as filtering from subsample_preds)
         sample_freq = []
+        print("sample set shape = {}, label = {}".format(sample_set.shape, sample_set_label))
         for i in range(num_clusters):
-            if(cluster_sizes[i] != 0):
-                sample_freq.append((sample_preds == i).sum() / cluster_sizes[i])
-            else:
-                # print("Cluster size = 0 for cluster {}, size = {}, (sample_preds==i).sum() = {}".format(i, cluster_sizes[i], (sample_preds == i).sum()))
-                sample_freq.append(0)
-
+            # Fraction of its samples clustered into cluster index "i"
+            print("cluster index = {}, # samples assigned = {}, % assigned = {}".format(i, (sample_set_preds == i).sum(), (sample_set_preds == i).sum() / sample_set.shape[0]))
+            sample_freq.append((sample_set_preds == i).sum() / sample_set.shape[0])
+        # print("Sample set frequency of {} = {}".format(sample_set_name, np.asarray(sample_freq).sum()))
         # Append sample set label
-        sample_labels.append(sample_label)
+        sample_labels.append(sample_set_label)
         vec.append(sample_freq)
     return np.asarray(vec), np.asarray(sample_labels), sample_sets
 
@@ -174,17 +174,14 @@ def get_classification_input(subsample_data, subsample_data2, train_sets, test_s
     return subsample_train_vec, subsample_train_labels, subsample_test_vec, subsample_test_labels
 
 
-def get_cluster_centers(subsample_data, train_sets, test_sets, num_clusters=15, method=2):
+def get_cluster_centers(subsample_data, train_sets, test_sets, num_clusters=15):
     """
     Getting KMeans cluster centers. Trying to run it multiple times to see if they vary with each run by comparing them on TSNE plots
     """
     subsample_train, subsample_test, subsample_train_X, subsample_train_Y, subsample_test_X, subsample_test_Y = get_subsample_train_test_data(subsample_data, train_sets, test_sets)
-    if (method == 2):
-        km = KMeans(init="k-means++", n_clusters=num_clusters, n_init=10)
-        subsample_train_preds = km.fit_predict(subsample_train_X)
-        return km.cluster_centers_
-    else:
-        print("Method != 2. Cannot run. Exiting...")
+    km = KMeans(init="k-means++", n_clusters=num_clusters, n_init=10)
+    subsample_train_preds = km.fit_predict(subsample_train_X)
+    return km.cluster_centers_
 
 
 def train_classifier(xtrain, ytrain, xtest, ytest, model_type='svm'):

scripts/sample_set_classification.py
@@ -34,8 +34,8 @@ def preprocess_input(input_data):
     # Creating label column from Sample Treatment
     req_hvtn_data.obs['label'] = req_hvtn_data.obs.Sample_Treatment.apply(lambda x: 1 if "GAG" in x else 0)
     # Transform using arcsinh
-    # req_hvtn_data.X = np.arcsinh((1. / 5) * req_hvtn_data.X)
-    req_hvtn_data.X = StandardScaler().fit_transform(req_hvtn_data.X)
+    req_hvtn_data.X = np.arcsinh((1. / 5) * req_hvtn_data.X)
+    # req_hvtn_data.X = StandardScaler().fit_transform(req_hvtn_data.X)
 
     return req_hvtn_data
 
@@ -150,27 +150,34 @@ def parallel_subsampling(fcs_filename):
 
 
 
-data_path = "/home/athreya/private/set_summarization/data/"
-output_data_path = "/playpen-ssd/athreya/set_summarization/data/hvtn"
-# data_path = "/home/athreya/private/set_summarization/data/preeclampsia"
+
 num_samples_per_set = 500
 
 if(proc == 'subsample'):
-    # Main
+    # Preprocess HVTN data
     # input_data = anndata.read_h5ad(data_path + "hvtn.h5ad")
     # standard_data = preprocess_input(input_data)
+    # standard_data.write(os.path.join(data_path, "hvtn_preprocessed.h5ad"))
 
-    # data = anndata.read_h5ad(os.path.join(data_path, "preeclampsia_preprocessed.h5ad"))
-    data = anndata.read_h5ad(os.path.join(data_path, "hvtn_preprocessed.h5ad"))
+    # output_data_path = "/playpen-ssd/athreya/set_summarization/data/hvtn"
+    output_data_path = "/playpen-ssd/athreya/set_summarization/data/preeclampsia"
+
+    data_path = "/home/athreya/private/set_summarization/data/"
+
+    data = anndata.read_h5ad(os.path.join(data_path, "preeclampsia_preprocessed.h5ad"))
+    # data = anndata.read_h5ad(os.path.join(data_path, "hvtn_preprocessed.h5ad"))
+
     print("Finished reading preprocessed data. Starting {} pools".format(num_processes))
+
     # KH subsampling
     fcs_file = data.obs.FCS_File.values.unique()[start:end]
+    # fcs_file = data.obs.FCS_File.values.unique()[[33, 24, 32, 25, 30, 7]]         # Run for specific sample sets in case proc failed in between
 
     lock = Lock()
 
     # Overwrite finished sample sets info from previous run with Current Run params
     with open(os.path.join(output_data_path, "finished_sets.txt"), 'w') as f:
-        f.write("Finished Sets for Scale Factor = {}, Iteration = {} -:".format(scale_factor, iteration))
+        f.write("Finished Sets for Scale Factor = {}, Iteration = {} -:\n".format(scale_factor, iteration))
 
     pool = Pool(processes=num_processes)
     pool.map(parallel_subsampling, fcs_file)
@@ -178,6 +185,10 @@ if(proc == 'subsample'):
 
 
 if(proc == 'merge'):
+
+    # output_data_path = "/playpen-ssd/athreya/set_summarization/data/hvtn"
+    output_data_path = "/playpen-ssd/athreya/set_summarization/data/preeclampsia"
+
     import shutil
     for method in ["iid", "kh", "hop", "geo"]:
         folder_path = os.path.join(output_data_path, "{}_samples".format(method))
@@ -185,6 +196,7 @@ if(proc == 'merge'):
 
         # Merge original subsample anndata
         output_file_name = "{}_subsamples_{}k_per_set_gamma{}x_{}.h5ad".format(method, num_samples_per_set / 1000, scale_factor, iteration)
+        print("Merging into {}".format(output_file_name))
         utils.merge_anndata(folder_path, file_regex, output_file_name)
         shutil.move(os.path.join(folder_path, output_file_name), os.path.join(output_data_path, output_file_name))
 
@@ -201,12 +213,20 @@ if(proc == 'merge'):
 if(proc == 'classify'):
     # KFold -> clustering -> cluster_freq vector -> classifier
     num_sketches = 3
-    num_clusters = 50
-    output_data_path = "/playpen-ssd/athreya/set_summarization/data/hvtn/loo_data"
-    # results_file = "classification_results_{}.csv".format(scale_factor)
+
+    # data_path = "/playpen-ssd/athreya/set_summarization/data/hvtn"
+    data_path = "/playpen-ssd/athreya/set_summarization/data/preeclampsia"
+
+    # output_data_path = "/playpen-ssd/athreya/set_summarization/data/hvtn/old_data"
+    # output_data_path = "/playpen-ssd/athreya/set_summarization/data/preeclampsia/loo_data"
+    # results_file = "5fold_cv_classification_results_{}.csv".format(scale_factor)
     # cross_validation(output_data_path, data_path, num_sketches, num_samples_per_set, results_file, scale_factor)
 
     # KH Leave One Out
+    num_clusters = 30
+
+    # output_data_path = "/playpen-ssd/athreya/set_summarization/data/hvtn/loo_data"
+    output_data_path = "/playpen-ssd/athreya/set_summarization/data/preeclampsia/loo_data"
     results_file = "loo_classification_results_kh_{}sketches_{}clusters.csv".format(num_sketches, num_clusters)
     leave_one_out_kh_validation(output_data_path, data_path, num_sketches, num_samples_per_set, num_processes, results_file, num_clusters)
 

scripts/train.py
@@ -19,7 +19,8 @@ from model import *
 
 
 def cross_validation(output_data_path, data_path, num_sketches, num_samples_per_set, results_file, scale_factor=None):
-    for sketch1, sketch2 in [(i ,j) for i in range(1 ,num_sketches+1) for j in range(1 ,num_sketches+1) if( i!=j)]:
+    for sketch1, sketch2 in [(i, j) for i in range(1, num_sketches+1) for j in range(1, num_sketches+1) if(i != j)]:
+    # for sketch1, sketch2 in [(1, 2),]:
         print("Reading data for sketch1 = {}, sketch2 = {}".format(sketch1, sketch2))
         kh_sample_data = anndata.read_h5ad(os.path.join(output_data_path, "kh_subsamples_{}k_per_set_gamma{}x_{}.h5ad".format(num_samples_per_set / 1000, scale_factor, sketch1)))
         iid_sample_data = anndata.read_h5ad(os.path.join(output_data_path, "iid_subsamples_{}k_per_set_gamma{}x_{}.h5ad".format(num_samples_per_set / 1000, scale_factor, sketch1)))
@@ -31,14 +32,14 @@ def cross_validation(output_data_path, data_path, num_sketches, num_samples_per_
         geo_sample_data2 = anndata.read_h5ad(os.path.join(output_data_path, "geo_subsamples_{}k_per_set_gamma{}x_{}.h5ad".format(num_samples_per_set / 1000, scale_factor, sketch2)))
         hop_sample_data2 = anndata.read_h5ad(os.path.join(output_data_path, "hop_subsamples_{}k_per_set_gamma{}x_{}.h5ad".format(num_samples_per_set / 1000, scale_factor, sketch2)))
 
-        for num_trials in range(30):
+        for num_trials in range(20):
             print("Starting trial {}".format(num_trials +1))
             kf5 = KFold(n_splits=5, shuffle=True)
             fcs_files = iid_sample_data.obs.FCS_File.values.unique()
 
             final_results = pd.DataFrame()
-            for method in (1, 2):
-            # for method in (2,):
+            # for method in (1, 2):
+            for method in (2,):
                 for num_clusters in (15, 30, 50):
                     results = []
                     print("Method = {}, # Clusters = {}".format(method, num_clusters))
@@ -49,7 +50,7 @@ def cross_validation(output_data_path, data_path, num_sketches, num_samples_per_
                         km = KMeans(init="k-means++", n_clusters=num_clusters, n_init=10)
                         iid_train_vec, iid_train_labels, iid_test_vec, iid_test_labels = get_classification_input(iid_sample_data, iid_sample_data2, train_sets, test_sets, num_clusters, km, method, is_iid=1)
                         model, acc, cf_matrix = train_classifier(iid_train_vec, iid_train_labels, iid_test_vec, iid_test_labels, model_type='svm')
-                        results.append([ i +1, "iid", acc])
+                        results.append([i + 1, "iid", acc])
 
                         # KH
                         kh_train_vec, kh_train_labels, kh_test_vec, kh_test_labels = get_classification_input(kh_sample_data, kh_sample_data2, train_sets, test_sets, num_clusters, km, method, is_iid=0)
@@ -127,53 +128,12 @@ def parallel_leaveoneout_kh_classification(test_set, num_clusters, kh_data):
     return best_model_ypred[1] == kh_test_labels, best_model_ypred[2], best_model_ypred[1]   # Prediction == GT, Gamma indicator to calculate pick rate, ypred
 
 
-def parallel_leaveoneout_others_classification(test_set, num_clusters, data):
-    """
-    Performs Leave-One-Out Classification and returns the ypred for test_set (For Geo, IID & Hopper subsamples)
-    :param test_set:
-    :param num_clusters:
-    :param kh_data:
-    :return:
-    """
-    print("{} process -: {}".format(current_process().pid, test_set))
-
-    iid_data, iid_data2, geo_data, geo_data2, hop_data, hop_data2 = data
-    method = 2
-    fcs_files = list(iid_data.obs.FCS_File.values.unique())
-
-    # Splitting out train and test sample set fcs files
-    train_sets = fcs_files[:]
-    train_sets.remove(test_set)
-    # KH
-    iteration_results = []
-    start = time.time()
-    
-    # IID
-    iid_train_vec, iid_train_labels, iid_test_vec, iid_test_labels = get_classification_input(iid_data, iid_data2, train_sets, [test_set], num_clusters, method=method, is_iid=0)
-    t1 = time.time()
-    model, best_estimator_score, ypred = leave_one_out_classifier(iid_train_vec, iid_train_labels, iid_test_vec, iid_test_labels, model_type='svm')
-    iteration_results.append(ypred == iid_test_labels)
-    print("Time for get_classification_input = {}, Time for Classifier = {}".format(t1-start, time.time()-t1))
-
-    # Geo
-    geo_train_vec, geo_train_labels, geo_test_vec, geo_test_labels = get_classification_input(geo_data, geo_data2, train_sets, [test_set], num_clusters, method=method, is_iid=0)
-    model, best_estimator_score, ypred = leave_one_out_classifier(geo_train_vec, geo_train_labels, geo_test_vec, geo_test_labels, model_type='svm')
-    iteration_results.append(ypred == geo_test_labels)
-
-    # Hopper
-    hop_train_vec, hop_train_labels, hop_test_vec, hop_test_labels = get_classification_input(hop_data, hop_data2, train_sets, [test_set], num_clusters, method=method, is_iid=0)
-    model, best_estimator_score, ypred = leave_one_out_classifier(hop_train_vec, hop_train_labels, hop_test_vec, hop_test_labels, model_type='svm')
-    iteration_results.append(ypred == hop_test_labels)
-
-    print("Finished {} in {} secs.".format(test_set, time.time()-start))
-
-    return iteration_results
-
 
 def leave_one_out_kh_validation(output_data_path, data_path, num_sketches, num_samples_per_set, num_processes, results_file, num_clusters):
+    print("Starting LOO validation for Kernel Herding sketches using {} processes and {} KMeans clusters".format(num_processes, num_clusters))
     pool = Pool(processes=num_processes)
-    for sketch1, sketch2 in [(i, j) for i in range(1, num_sketches+1) for j in range(1, num_sketches+1) if(i != j)]:
-    # for sketch1, sketch2 in [(1,3),]:
+    # for sketch1, sketch2 in [(i, j) for i in range(1, num_sketches+1) for j in range(1, num_sketches+1) if(i != j)]:
+    for sketch1, sketch2 in [(1,2),]:
         print("Reading data for sketch1 = {}, sketch2 = {}".format(sketch1, sketch2))
 
         # Load sketches 1&2 for each gamma value
@@ -204,7 +164,7 @@ def leave_one_out_kh_validation(output_data_path, data_path, num_sketches, num_s
             ypred_positive_count = np.sum(pool_results[:, 2])                                           # Positive ypred Count (ypred == 1)
             print(gamma_pick_rate)
 
-            results.append([sketch1, sketch2, num_trials + 1, "kh", acc, gamma_pick_rate, ypred_positive_count])
+            results.append([sketch1, sketch2, num_trials + 1, "kh_hyperparam", acc, gamma_pick_rate, ypred_positive_count])
             final_results = pd.DataFrame(results, columns=["Sketch1", "Sketch2", "Trial #", "Method", "Acc", "Gamma Pick Rate", "ypred +ve Count"])
             print("Finished for trial {}. Writing to file".format(num_trials + 1))
             classification_results_file = os.path.join(data_path, results_file)
@@ -213,26 +173,80 @@ def leave_one_out_kh_validation(output_data_path, data_path, num_sketches, num_s
             else:
                 final_results.to_csv(classification_results_file, index=False)
 
-            print("Time taken for trial {} = {} secs".format(num_trials, time.time()-start))
+            print("Time taken for trial {} = {} secs".format(num_trials + 1, time.time()-start))
 
     pool.close()
     pool.join()
     pool.terminate()
-    
-    
+
+
+def parallel_leaveoneout_others_classification(test_set, num_clusters, data):
+    """
+    Performs Leave-One-Out Classification and returns the ypred for test_set (For Geo, IID & Hopper subsamples)
+    :param test_set:
+    :param num_clusters:
+    :param kh_data:
+    :return:
+    """
+    print("{} process -: {}".format(current_process().pid, test_set))
+
+    iid_data, iid_data2, geo_data, geo_data2, hop_data, hop_data2, kh_data, kh_data2 = data
+    method = 2
+    fcs_files = list(iid_data.obs.FCS_File.values.unique())
+
+    # Splitting out train and test sample set fcs files
+    train_sets = fcs_files[:]
+    train_sets.remove(test_set)
+    # print("Length of train_sets = {}".format(len(train_sets)))
+    # KH
+    iteration_results = []
+    start = time.time()
+
+    # IID
+    iid_train_vec, iid_train_labels, iid_test_vec, iid_test_labels = get_classification_input(iid_data, iid_data2, train_sets, [test_set], num_clusters, method=method, is_iid=0)
+    t1 = time.time()
+    model, best_estimator_score, ypred = leave_one_out_classifier(iid_train_vec, iid_train_labels, iid_test_vec, iid_test_labels, model_type='svm')
+    iteration_results.append(ypred == iid_test_labels)
+    print("Time for get_classification_input = {}, Time for Classifier = {}".format(t1-start, time.time()-t1))
+
+    # Geo
+    geo_train_vec, geo_train_labels, geo_test_vec, geo_test_labels = get_classification_input(geo_data, geo_data2, train_sets, [test_set], num_clusters, method=method, is_iid=0)
+    model, best_estimator_score, ypred = leave_one_out_classifier(geo_train_vec, geo_train_labels, geo_test_vec, geo_test_labels, model_type='svm')
+    iteration_results.append(ypred == geo_test_labels)
+
+    # Hopper
+    hop_train_vec, hop_train_labels, hop_test_vec, hop_test_labels = get_classification_input(hop_data, hop_data2, train_sets, [test_set], num_clusters, method=method, is_iid=0)
+    model, best_estimator_score, ypred = leave_one_out_classifier(hop_train_vec, hop_train_labels, hop_test_vec, hop_test_labels, model_type='svm')
+    iteration_results.append(ypred == hop_test_labels)
+
+    # KH with same gamma (1x) instead of picking between diff gammas
+    kh_train_vec, kh_train_labels, kh_test_vec, kh_test_labels = get_classification_input(kh_data, kh_data2, train_sets, [test_set], num_clusters, method=method, is_iid=0)
+    model, best_estimator_score, ypred = leave_one_out_classifier(kh_train_vec, kh_train_labels, kh_test_vec, kh_test_labels, model_type='svm')
+    iteration_results.append(ypred == kh_test_labels)
+
+    print("Finished {} in {} secs.".format(test_set, time.time()-start))
+
+    return iteration_results
+
+
+
 def leave_one_out_others_validation(output_data_path, data_path, num_sketches, num_samples_per_set, num_processes, results_file, num_clusters):
+    print("Starting LOO validation for IID, Geo, Hopper and KH (only 1 gamma) sketches using {} processes and {} KMeans clusters".format(num_processes, num_clusters))
     pool = Pool(processes=num_processes)
-    for sketch1, sketch2 in [(i, j) for i in range(1, num_sketches+1) for j in range(1, num_sketches+1) if(i != j)]:
+    # for sketch1, sketch2 in [(i, j) for i in range(1, num_sketches+1) for j in range(1, num_sketches+1) if(i != j)]:
+    for sketch1, sketch2 in [(1,2), (2,1)]:
         print("Reading data for sketch1 = {}, sketch2 = {}".format(sketch1, sketch2))
 
         # Load sketches 1&2 for each of the methods
         iid_data = anndata.read_h5ad(os.path.join(output_data_path, "iid_subsamples_{}k_per_set_gamma{}x_{}.h5ad".format(num_samples_per_set / 1000, 1.0, sketch1)))
         geo_data = anndata.read_h5ad(os.path.join(output_data_path, "geo_subsamples_{}k_per_set_gamma{}x_{}.h5ad".format(num_samples_per_set / 1000, 1.0, sketch1)))
         hop_data = anndata.read_h5ad(os.path.join(output_data_path, "hop_subsamples_{}k_per_set_gamma{}x_{}.h5ad".format(num_samples_per_set / 1000, 1.0, sketch1)))
+        kh_data = anndata.read_h5ad(os.path.join(output_data_path, "kh_subsamples_{}k_per_set_gamma{}x_{}.h5ad".format(num_samples_per_set / 1000, 1.0, sketch1)))
 
         iid_data2 = anndata.read_h5ad(os.path.join(output_data_path, "iid_subsamples_{}k_per_set_gamma{}x_{}.h5ad".format(num_samples_per_set / 1000, 1.0, sketch2)))
         geo_data2 = anndata.read_h5ad(os.path.join(output_data_path, "geo_subsamples_{}k_per_set_gamma{}x_{}.h5ad".format(num_samples_per_set / 1000, 1.0, sketch2)))
         hop_data2 = anndata.read_h5ad(os.path.join(output_data_path, "hop_subsamples_{}k_per_set_gamma{}x_{}.h5ad".format(num_samples_per_set / 1000, 1.0, sketch2)))
+        kh_data2 = anndata.read_h5ad(os.path.join(output_data_path, "kh_subsamples_{}k_per_set_gamma{}x_{}.h5ad".format(num_samples_per_set / 1000, 1.0, sketch2)))
 
 
         # Re-run N times
@@ -243,16 +257,16 @@ def leave_one_out_others_validation(output_data_path, data_path, num_sketches, n
 
             results = []
 
-            pool_results = pool.map(partial(parallel_leaveoneout_others_classification, num_clusters=num_clusters, data=(iid_data, iid_data2, geo_data, geo_data2, hop_data, hop_data2)), fcs_files, chunksize=len(fcs_files)//num_processes)
-            print(pool_results)
+            pool_results = pool.map(partial(parallel_leaveoneout_others_classification, num_clusters=num_clusters, data=(iid_data, iid_data2, geo_data, geo_data2, hop_data, hop_data2, kh_data, kh_data2)), fcs_files, chunksize=len(fcs_files)//num_processes)
             # Aggregate the results from each of the workers
             pool_results = np.asarray(pool_results)
-            print(pool_results.shape, len(fcs_files))
+            print("pool_results.shape = {}, len(fcs_files) = {}, pool_results = {}".format(pool_results.shape, len(fcs_files), pool_results))
             acc = np.sum(pool_results, axis=0) / len(fcs_files)
 
-            results.append([sketch1, sketch2, num_trials + 1, "iid", acc[0]])
-            results.append([sketch1, sketch2, num_trials + 1, "geo", acc[1]])
-            results.append([sketch1, sketch2, num_trials + 1, "hop", acc[2]])
+            results.append([sketch1, sketch2, num_trials + 1, "iid", acc[0][0]])
+            results.append([sketch1, sketch2, num_trials + 1, "geo", acc[1][0]])
+            results.append([sketch1, sketch2, num_trials + 1, "hop", acc[2][0]])
+            results.append([sketch1, sketch2, num_trials + 1, "kh", acc[3][0]])
             final_results = pd.DataFrame(results, columns=["Sketch1", "Sketch2", "Trial #", "Method", "Acc"])
             print("Finished for trial {}. Writing to file".format(num_trials + 1))
             classification_results_file = os.path.join(data_path, results_file)