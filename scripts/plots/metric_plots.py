22ca00693501bb98851cd7d0860d248e1b28abc4
Vishal <bvishal.athreya@gmail.com>
2022-05-13 12:41:02 -0700

    Fix ylim for plots

--- scripts/plots/metric_plots.py
+++ scripts/plots/metric_plots.py
@@ -49,7 +49,7 @@ def rfe_plot():
     g.set_xlabel("Number of Sampled Cells per set", size=16)
     g.set_ylabel("Log (Mean L1 Distance of RFE Values) \n Between Original and Sketched Sample-Sets", size=16)
     plt.legend(bbox_to_anchor=(1.01, 1), loc=2, borderaxespad=0., prop={'size': 12})
-    # g.set_ylim([-4.5, 2.5])
+    g.set_ylim([-4.5, 4])
     g.xaxis.set_tick_params(labelsize=18)
     g.yaxis.set_tick_params(labelsize=18)
     # plt.show()
@@ -64,7 +64,7 @@ def singular_values_plot():
     sv_melt = sv.melt(id_vars=['subsamples', 'Sample Set'])
     sv_melt['log_value'] = np.log(sv_melt['value'])
 
-    _, axes = plt.subplots(1, 1, figsize=(6, 5))
+    _, axes = plt.subplots(1, 1, figsize=(12, 5))
     g = sns.lineplot(x="subsamples", y="log_value", hue='variable', data=sv_melt, style='variable',
                      dashes=[""] * len(methods), markers=["o"] * len(methods), palette=colors_dict,
                      ci='sd', err_style='bars', ax=axes, err_kws={'capsize': 6})
@@ -73,7 +73,7 @@ def singular_values_plot():
     g.set_xlabel("Number of Sketched Cells", size=16)
     g.set_ylabel("Log (Mean L1 Distance of Singular Values) \n Between Original and Sketched Sample-Sets", size=16)
     plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0., prop={'size': 14})
-    g.set_ylim([-4, 4])
+    g.set_ylim([-4, 4.5])
     g.xaxis.set_tick_params(labelsize=18)
     g.yaxis.set_tick_params(labelsize=18)
     # plt.show()
